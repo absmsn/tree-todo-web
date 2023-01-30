bbef145454aa2dd8ca413ebd1ca84bfd7fe7c0bc
hanzhao <17734085891@163.com>
2023-01-30 22:57:31 +0800

    feat: 通过配置文件设置后端api地址。

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const localBaseURL = "http://localhost:3005/api";
+const baseURL = process.env.NODE_ENV === "production" 
+  ? (process.env.REACT_APP_API_PREFIX ?? localBaseURL)
+  : localBaseURL
+
 const instance = axios.create({
-  baseURL: "http://localhost:3005/api",
+  baseURL: baseURL,
   validateStatus(status) {
     return status < 400;
   }
