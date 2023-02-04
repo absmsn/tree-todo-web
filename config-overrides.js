057a380e62b8b27ffe14185469c1a5466e25eebd
hanzhao <17734085891@163.com>
2023-02-04 21:36:26 +0800

    chore: 增加对打包后文件大小的分析。

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,20 @@
-const { override, addWebpackAlias } = require("customize-cra");
+const { override, addWebpackAlias, addWebpackPlugin } = require("customize-cra");
+const ProgressBarPlugin = require("progress-bar-webpack-plugin");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 const path = require("path");
  
+const addAnalyzer = () => config => {
+  if (process.env.ANALYZER) {
+    config.plugins.push(new BundleAnalyzerPlugin());
+  }
+  return config;
+};
+
 module.exports = override(
+  addAnalyzer(),
+  addWebpackPlugin(
+    new ProgressBarPlugin()
+  ),
   addWebpackAlias({
     "@": path.resolve(__dirname, "src")
   })
